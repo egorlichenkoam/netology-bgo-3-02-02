5eb781e1310f78a5b0210e8bc00365909c7f90ba
egorlichenkoam <egorlichenkoam@gmail.com>
2020-12-27 21:37:34 +0500

    Transfer service

--- pkg/card/card.go
+++ pkg/card/card.go
@@ -1,84 +1,47 @@
 package card
 
-import "01/pkg/money"
+import (
+	"01/pkg/money"
+	"math/rand"
+)
+
+type Currency string
+
+const (
+	RUB Currency = "RUB"
+)
 
 type Card struct {
-	Id           int64
-	Issuer       string
-	Balance      int64
-	Currency     string
-	Number       string
-	Icon         string
-	Transactions []Transaction
+	Id       int64
+	Issuer   string
+	Balance  money.Money
+	Currency Currency
+	Number   string
+	Icon     string
+}
+
+func (s *Service) NewCard(issuer string, balance money.Money, currency Currency, number string) *Card {
+	return s.Add(Card{Id: rand.Int63(), Issuer: issuer, Balance: balance, Currency: currency, Number: number, Icon: ""})
 }
 
 type Service struct {
-	Cards []*Card
+	Cards []Card
 }
 
 func NewService() *Service {
 	return &Service{}
 }
 
-func AddTransaction(card *Card, transaction *Transaction) {
-	if transaction != nil {
-		card.Transactions = append(card.Transactions, *transaction)
-	}
+func (s *Service) Add(card Card) *Card {
+	s.Cards = append(s.Cards, card)
+	return &card
 }
 
-func SumByMcc(transactions []Transaction, mccs []Mcc) money.Money {
-	var result money.Money = 0
-	transactions = filterTransactionsByMcc(transactions, mccs)
-	for _, transaction := range transactions {
-		result = result + transaction.Amount
-	}
-	return result
-}
-
-func filterTransactionsByMcc(transactions []Transaction, mccs []Mcc) []Transaction {
-	result := make([]Transaction, 0)
-	for _, transaction := range transactions {
-		for _, mcc := range mccs {
-			if transaction.Mcc == mcc {
-				result = append(result, transaction)
-			}
+func (s *Service) ByNumber(number string) (card *Card) {
+	for i, c := range s.Cards {
+		if c.Number == number {
+			return &s.Cards[i]
 		}
 	}
-	return result
-}
-
-func TranslateMcc(code Mcc) string {
-	result := "Категория не указана"
-	value, ok := Mccs()[code]
-	if ok {
-		result = value
-	}
-	return result
-}
-
-func LastNTransactions(card *Card, n int) []Transaction {
-	if len(card.Transactions) < n {
-		n = len(card.Transactions)
-	}
-	nTransactions := make([]Transaction, n)
-	n = len(card.Transactions) - n
-	copy(nTransactions, card.Transactions[n:len(card.Transactions)])
-	for i := len(nTransactions)/2 - 1; i >= 0; i-- {
-		flipIdx := len(nTransactions) - 1 - i
-		nTransactions[i], nTransactions[flipIdx] = nTransactions[flipIdx], nTransactions[i]
-	}
-	return nTransactions
-}
-
-func LastNTransactions2(card *Card, n int) []Transaction {
-	if len(card.Transactions) < n {
-		n = len(card.Transactions)
-	}
-	n = len(card.Transactions) - n
-	nTransactions := card.Transactions[n:len(card.Transactions)]
-	for i := len(nTransactions)/2 - 1; i >= 0; i-- {
-		flipIdx := len(nTransactions) - 1 - i
-		nTransactions[i], nTransactions[flipIdx] = nTransactions[flipIdx], nTransactions[i]
-	}
-	return nTransactions
+	return nil
 }
